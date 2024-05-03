6e356416209a93411a318a3a2fcc4fbe8bbb5102
AnkushinDaniil <ankushin.daniil42@gmail.com>
2024-05-03 16:02:28 +0300

    3 hw done

--- 3/99_hw/fast.go
+++ 3/99_hw/fast.go
@@ -1,18 +1,87 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"io"
+	"os"
+	"strconv"
+	"strings"
+
+	"hw3/user"
 )
 
-// вам надо написать более быструю оптимальную этой функции
+type User = user.User
+
 func FastSearch(out io.Writer) {
-	/*
-		!!! !!! !!!
-		обратите внимание - в задании обязательно нужен отчет
-		делать его лучше в самом начале, когда вы видите уже узкие места, но еще не оптимизировалм их
-		так же обратите внимание на команду в параметром -http
-		перечитайте еще раз задание
-		!!! !!! !!!
-	*/
-	SlowSearch(out)
+	out.Write([]byte("found users:\n"))
+
+	file, err := os.Open(filePath)
+	defer file.Close()
+
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	var (
+		notSeenBefore, isAndroid, isMSIE bool
+		browser                          string
+		data                             []byte
+	)
+
+	seenBrowsers := make([]string, 0, 128)
+	uniqueBrowsers := 0
+
+	help := func() {
+		notSeenBefore = true
+		for _, item := range seenBrowsers {
+			if item == browser {
+				notSeenBefore = false
+			}
+		}
+		if notSeenBefore {
+			seenBrowsers = append(seenBrowsers, browser)
+			uniqueBrowsers++
+		}
+	}
+
+	var user User
+
+	i := 0
+	for scanner.Scan() {
+		data = scanner.Bytes()
+		user.UnmarshalJSON(data)
+		// err := json.Unmarshal(data, &user)
+		isAndroid, isMSIE = false, false
+
+		for _, browser = range user.Browsers {
+			if strings.Contains(browser, "Android") {
+				isAndroid = true
+				help()
+			}
+			if strings.Contains(browser, "MSIE") {
+				isMSIE = true
+				help()
+			}
+		}
+
+		if !(isAndroid && isMSIE) {
+			i++
+			continue
+		}
+
+		out.Write([]byte{'['})
+		out.Write([]byte(strconv.Itoa(i)))
+		out.Write([]byte{']', ' '})
+		out.Write([]byte(user.Name))
+		out.Write([]byte{' ', '<'})
+		out.Write([]byte(strings.ReplaceAll(user.Email, "@", " [at] ")))
+		out.Write([]byte{'>', '\n'})
+
+		i++
+	}
+
+	fmt.Fprintln(out, "\nTotal unique browsers", len(seenBrowsers))
 }
